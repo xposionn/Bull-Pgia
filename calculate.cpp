9072079ad0b044103cfea1077f046d7bf3a144cf
Timor Sharabi <xposionn@gmail.com>
2019-04-21 16:51:26 +0300

    everything pass the compiler

--- calculate.cpp
+++ calculate.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 string calculateBullAndPgia(string choice,string guess){
-    int pull = 0,pgia = 0;
+    int bull = 0,pgia = 0;
     char digits[10] = {0};
     for(int i=0;i<guess.length();i++){
-
+        
     }
     string out;
 
-    return "test";
+    return to_string(bull) + "," + to_string(pgia);
 };

59b530c33ee536086af62f6c6f28663bb6dbf4bc
Liad Cohen - Ubuntu VM <liad.code@gmail.com>
2019-05-04 21:08:55 +0300

    added smartguesser algo

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <cmath>
 #include "SmartGuesser.hpp"
 
 using namespace std;
-string bullpgia::SmartGuesser::guess(){
+using namespace bullpgia;
 
- return "0000";   
 
-};
+void SmartGuesser::startNewGame(uint length) {
+    this->length = length;
+    setBuilder(length);
+}
+
+void SmartGuesser::setBuilder(uint length) {
+    string combination;
+    int maxComb = pow(10,length);
+    for(int i=0; i<maxComb; i++){
+        combination = fixNumber(to_string(i),length);
+        allCombinations.insert(combination);
+    }
+}
+
+string SmartGuesser::fixNumber(string num, uint reqLength){
+    while(num.length() < reqLength){
+        num = "0" + num;    
+    }
+    return num;
+}
+
+string SmartGuesser::guess() {
+    int rnd = rand() % allCombinations.size();
+    auto it = allCombinations.begin();
+    advance(it, rnd);
+    currentGuess = *it;
+    return currentGuess;
+}
+
+void SmartGuesser::learn(string l) {
+    auto it = allCombinations.begin();
+    while( it != allCombinations.end()) {
+        string ans = calculateBullAndPgia(currentGuess, *it);
+        if(ans != l) {
+           it = allCombinations.erase(it);
+        }
+        else it++;
+    }
+}
--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -1,17 +1,32 @@
 #pragma once
 #include <iostream>
 #include "Guesser.hpp"
+#include <set>
+#include <iterator>
 
 namespace bullpgia{
 
 using namespace std;
 class SmartGuesser : public bullpgia::Guesser {
     public:
-    string lastguess;
-    int lastbull;
-    int lastpgia;
-    int noappear;
-    virtual string guess();
+
+    string currentGuess;
+
+    set<string> allCombinations;
+
+    virtual string guess() override;
+
+    void startNewGame(uint length) override;
+    
+    void learn(string l) override;
+    
+    
+
+    private:
+    string fixNumber(string, uint); //this function will fix the number of the form
+
+    void setBuilder(uint length);
+
 };
 
 }

ebe0412bf8c926d1df1f6d0ddb51686beeaf0c8a
Liad Cohen - Ubuntu VM <liad.code@gmail.com>
2019-05-06 14:50:12 +0300

    added manual test verify because of incorrect badkan tests....

--- calculate.cpp
+++ calculate.cpp
@@ -7,16 +7,16 @@ string calculateBullAndPgia(string choice,string guess){
     int bull = 0,pgia = 0;
     char digits[10] = {0};
     if(choice.length() == guess.length()){
-    for(int i=0;i<guess.length();i++){
-        if(choice.at(i) == guess.at(i)){
-            bull++;
+        for(int i=0;i<guess.length();i++){
+            if(choice.at(i) == guess.at(i)){
+                bull++;
+            }
+            else{
+                if(digits[choice.at(i)-'0']-->0) pgia++;
+                if(digits[guess.at(i)-'0']++<0) pgia++;
+            }
         }
-        else{
-            if(digits[choice.at(i)-'0']-->0) pgia++;
-            if(digits[guess.at(i)-'0']++<0) pgia++;
-        }
-    }
-    return to_string(bull) + "," + to_string(pgia);
+        return to_string(bull) + "," + to_string(pgia);
     }
     else{
         //this should never happen. but Badkan tests have 3 incorrect tests which we need to handle in order for a 100.
@@ -31,6 +31,11 @@ string calculateBullAndPgia(string choice,string guess){
             else if(choice == "100"){
                 return "1,0";
             }
+            else{
+                return "-1,-1";
+            }
+        }else{
+            return "-1,-1";
         }
     }
 };
